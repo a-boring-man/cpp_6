c28b86c7795cc4179688215083cf844c1c3fa924
jonathan <imaginary@email.com>
2022-10-13 15:22:16 +0200

    almost finish

--- ex00/include/main.hpp
+++ ex00/include/main.hpp
@@ -6,7 +6,7 @@
 /*   By: jrinna <jrinna@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:52:38 by jrinna            #+#    #+#             */
-/*   Updated: 2022/10/11 12:57:52 by jrinna           ###   ########lyon.fr   */
+/*   Updated: 2022/10/13 15:16:43 by jrinna           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 # include <iostream>
 # include <string>
+# include <limits.h>
+# include <float.h>
 
 using std::cout;
 using std::endl;
--- ex00/source/main.cpp
+++ ex00/source/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jrinna <jrinna@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:52:42 by jrinna            #+#    #+#             */
-/*   Updated: 2022/10/13 09:57:44 by jrinna           ###   ########lyon.fr   */
+/*   Updated: 2022/10/13 15:21:40 by jrinna           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ int	ft_is_float( string input ){
 		else
 			cs = -1;
 	}
-	if (cs > 6)
+	if (cs > 6 || !input.compare("-inff") || !input.compare("+inff") || !input.compare("nanf"))
 		return (1);
 	return (0);
 }
@@ -88,9 +88,217 @@ int	ft_is_double( string input ){
 		else
 			cs = -1;
 	}
-	if (cs > 4)
+	if (cs > 4 || !input.compare("-inf") || !input.compare("+inf") || !input.compare("nan") )
 		return (1);
 	return (0);
+}
+
+void	ft_display_char(int i, string input) {
+
+	int		ic;
+	float	fc;
+	float	dc;
+	switch (i)
+	{
+	case 0: // if already char
+		cout << "base type : char : '" << input.at(1) << "'" << endl;
+		break;
+
+	case 1: // if int to char
+		try
+		{
+			stoi(input);
+		}
+		catch(const std::exception& e)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		ic = stoi(input);
+		if ((ic > 31 && ic < 127) || (ic > 8 && ic < 14))
+			cout << "char : '" << static_cast<char>(ic) << "'" << endl;
+		else if (ic > 255 || ic < 0)
+			cout << "impossible" << endl;
+		else
+			cout << "Non displayable" << endl;
+		break;
+
+	case 2: // if float to char
+		try
+		{
+			stof(input);
+		}
+		catch(const std::exception& e)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		fc = stof(input);
+		if ((fc > 31 && fc < 127) || (fc > 8 && fc < 14))
+			cout << "char : '" << static_cast<char>(fc) << "'" << endl;
+		else if (fc > 255 || fc < 0)
+			cout << "impossible" << endl;
+		else
+			cout << "Non displayable" << endl;
+		break;
+
+	case 3: // if double to char
+		try
+		{
+			stod(input);
+		}
+		catch(const std::exception& e)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		dc = stod(input);
+		if ((dc > 31 && dc < 127) || (dc > 8 && dc < 14))
+			cout << "char : '" << static_cast<char>(dc) << "'" << endl;
+		else if (dc > 255 || dc < 0)
+			cout << "impossible" << endl;
+		else
+			cout << "Non displayable" << endl;
+		break;
+
+	default:
+		break;
+	}
+	return;
+	
+}
+
+void	ft_display_int(int i, string input) {
+
+	int		ii;
+	float	fi;
+	double	di;
+	switch (i)
+	{
+	case 0: // char to int
+		cout << "int : " << static_cast<int>(input.at(1)) << endl;
+		break;
+	
+	case 1: // int to int
+		try
+		{
+			stoi(input);
+		}
+		catch(const std::exception& e)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		ii = stoi(input);
+		cout << "base type : int : " << ii << endl;
+		break;
+
+	case 2: // float to int
+		try
+		{
+			stof(input);
+		}
+		catch(const std::exception& e)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		fi = stof(input);
+		if (fi < INT_MIN || fi > INT_MAX)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		cout << "int : " << static_cast<int>(fi) << endl;
+		break;
+
+	case 3: // double to int
+		try
+		{
+			stod(input);
+		}
+		catch(const std::exception& e)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		di = stod(input);
+		if (di < -2147483648 || di > 2147483647)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		cout << "int : " << static_cast<int>(di) << endl;
+		break;
+
+	default:
+		break;
+	}
+	return;
+}
+
+void	ft_display_float(int i, string input) {
+
+	int		itf;
+	float	ftf;
+	double	dtf;
+	switch (i)
+	{
+	case 0: // char to float
+		cout << "float : " << static_cast<float>(input.at(1)) << endl;
+		break;
+	
+	case 1: // int to float
+		try
+		{
+			stof(input);
+		}
+		catch(const std::exception& e)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		itf = stof(input);
+		cout << "float : " << itf << endl;
+		break;
+
+	case 2: // float to float
+		try
+		{
+			stof(input);
+		}
+		catch(const std::exception& e)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		ftf = stof(input);
+		cout << "base type : float : " << ftf << endl;
+		break;
+
+	case 3: // double to float
+		try
+		{
+			stod(input);
+		}
+		catch(const std::exception& e)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		dtf = stod(input);
+		if (dtf < -FLT_MAX || dtf > FLT_MAX)
+		{
+			cout << "impossible" << endl;
+			break;
+		}
+		cout << "float : " << static_cast<float>(dtf) << endl;
+		break;
+
+	default:
+		break;
+	}
+	return;
 }
 
 int main( int ac, char **av ) {
@@ -100,9 +308,12 @@ int main( int ac, char **av ) {
 		cout << "please use only one string as input" << endl;
 		return (1);
 	}
-	int (* f [4])(string input) = {&ft_is_char, &ft_is_int, &ft_is_float, &ft_is_double};
+
+	int 	(* f [4])(string input) = {&ft_is_char, &ft_is_int, &ft_is_float, &ft_is_double};
 	string	input = av[1];
 	bool	result[4] = {0, 0, 0, 0};
+	int 	i = 0;
+
 	for (int i = 0; i < 4; i++)
 	{
 		if ( f[i]( input ) )
@@ -110,10 +321,17 @@ int main( int ac, char **av ) {
 			result[i] = 1;
 		}
 	}
-	for (int i = 0; i < 4; i++)
+	while (i < 4 && !result[i])
+		i++;
+	if (i == 4)
 	{
-		cout << result[i] << endl;
+		cout << "type is not recognize/supported" << endl;
+		return (1);
 	}
+	ft_display_char(i, input);
+	ft_display_int(i, input);
+	ft_display_float(i, input);
+	//ft_display_double(result, input);
 
 	return (0);
 }
