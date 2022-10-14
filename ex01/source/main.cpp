510c598bf63d56a0082a6f19352db1577454aafa
jonathan <imaginary@email.com>
2022-10-14 13:28:53 +0200

    ex01 finish

---
+++ ex01/source/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrinna <jrinna@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 11:52:42 by jrinna            #+#    #+#             */
+/*   Updated: 2022/10/14 13:21:45 by jrinna           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/main.hpp"
+
+uintptr_t serialize(Data* ptr) {
+
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data* deserialize(uintptr_t raw) {
+
+	return (reinterpret_cast<Data*>(raw));
+}
+
+int main( void ) {
+
+	Data 		D;
+	uintptr_t	P;
+	P = serialize(&D);
+	cout << &D << endl << D.a << endl << D.b << endl;
+	cout << P << endl;
+	cout << deserialize(P) << endl << deserialize(P)->a << endl << deserialize(P)->b << endl;
+	return (0);
+}
